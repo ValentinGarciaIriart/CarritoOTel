167e9678bcb25f4aeb40fd06a8387edbdbe91eeb
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-07-26 10:24:08 -0300

    267

--- docker-build.sh
+++ docker-build.sh
@@ -8,14 +8,14 @@ echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdi
 
 
 # Construir y empujar las imágenes Docker con las etiquetas correctas
-docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-apache:otel3" apache
-docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-apache:otel3"
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-apache:otel4" apache
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-apache:otel4"
 
-docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-catalog:otel3" microservice-kubernetes-demo-catalog
-docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-catalog:otel3"
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-catalog:otel4" microservice-kubernetes-demo-catalog
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-catalog:otel4"
 
-docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-customer:otel3" microservice-kubernetes-demo-customer
-docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-customer:otel3"
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-customer:otel4" microservice-kubernetes-demo-customer
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-customer:otel4"
 
-docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-order:otel3" microservice-kubernetes-demo-order
-docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-order:otel3"
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-order:otel4" microservice-kubernetes-demo-order
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-order:otel4"
